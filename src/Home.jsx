4b4217b2a8ca50cb755df5734e5a5eb286683891
Ubuntu <ubuntu@ip-172-26-12-160.ec2.internal>
2023-11-29 01:42:14 +0000

    Added gtg to home page with useeffect

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import "./App.css";
 import Container from 'react-bootstrap/Container';
 import Col from 'react-bootstrap/Col';
